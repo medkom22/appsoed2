fd5e1a8fc35c6fa5c6b569a11116463c8fb46ec7
medkom22 <105618392+medkom22@users.noreply.github.com>
2022-05-18 20:01:42 +0700

    memberikan komentar

--- lib/example.dart
+++ lib/example.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ExampleApp extends StatelessWidget {
   const ExampleApp({Key? key}) : super(key: key);
-
+//menambahkan komen pada app
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
