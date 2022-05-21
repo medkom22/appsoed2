e384e4844fc3eea4c5177e91e3b361ab408c08f6
Handika Rafli Aryatama <76081296+handika231@users.noreply.github.com>
2022-05-21 21:52:40 +0700

    Page Sign In = Clear

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:appsoed/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -13,11 +14,18 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            child: const Text('Sign In'),
+            onPressed: () {
+              Get.toNamed(Routes.SIGNIN);
+            },
+          )
+        ],
+      )),
     );
   }
 }
