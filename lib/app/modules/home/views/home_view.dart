517aefa5c6db06ed24aa6d3842e88eaf0361e22d
Handika Rafli Aryatama <76081296+handika231@users.noreply.github.com>
2022-05-28 11:30:42 +0700

    rename file assets

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,38 +6,29 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ElevatedButton(
-            child: const Text('Sign In'),
-            onPressed: () {
-              Get.toNamed(Routes.SIGNIN);
-            },
+        body: SafeArea(
+            child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24, top: 26),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Selamat datang,\nNama User!',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              )
+            ],
           ),
-          ElevatedButton(
-            child: const Text('Profile Kabinet'),
-            onPressed: () {
-              Get.toNamed(Routes.PROFILE_KABINET);
-            },
-          ),
-          ElevatedButton(
-            child: const Text('About Appsoed'),
-            onPressed: () {
-              Get.toNamed(Routes.ABOUT_APPSOED);
-            },
-          )
-        ],
-      )),
-    );
+        )
+      ],
+    )));
   }
 }
