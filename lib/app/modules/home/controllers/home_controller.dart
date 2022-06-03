65f14185407c8ae4f105fdc1bf8517513e753415
Handika Rafli Aryatama <76081296+handika231@users.noreply.github.com>
2022-06-03 07:47:17 +0700

    fix bottomnavbar

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,10 +2,14 @@ import 'package:carousel_slider/carousel_controller.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  var current = 0.obs;
+  var currentSlider = 0.obs;
   final CarouselController carouselController = CarouselController();
+  var currentBottom = 0.obs;
+  onPageChangeBottom(int index) {
+    currentBottom.value = index;
+  }
 
-  onPageChange(index, reason) {
-    current.value = index;
+  onPageChangeSlider(index, reason) {
+    currentSlider.value = index;
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:appsoed/app/routes/app_pages.dart';
 import 'package:badges/badges.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -11,17 +13,96 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   HomeView({Key? key}) : super(key: key);
-  final List<String> imgList = [
-    'assets/S3.png',
-    'assets/PORSOED.png',
-    'assets/nyala_desa.png',
-    'assets/FOSA.png',
+
+  final List<Widget> widgetMenuPages = [
+    HomeWidget(),
+    Container(
+      color: Colors.amber,
+    ),
+    Container(
+      color: Colors.lightBlue,
+    ),
+    Container(
+      color: Colors.lightGreen,
+    ),
+    Container(
+      color: Colors.cyan,
+    ),
   ];
 
-  final int _current = 0;
-  final CarouselController _controller = CarouselController();
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      // NOTE: BOTTOM NAVIGATION BAR
+      bottomNavigationBar: Obx(() {
+        return CurvedNavigationBar(
+          animationCurve: Curves.easeInToLinear,
+          color: const Color(0xffD8D8D8).withOpacity(0.5),
+          buttonBackgroundColor: Colors.amber,
+          index: controller.currentBottom.value,
+          onTap: controller.onPageChangeBottom,
+          backgroundColor: Colors.white,
+          height: 50,
+          items: [
+            Icon(
+              Icons.home,
+              size: 30,
+              color: controller.currentBottom.value == 0
+                  ? Colors.white
+                  : const Color(0xffFDB731),
+            ),
+            Icon(
+              Icons.newspaper,
+              size: 30,
+              color: controller.currentBottom.value == 1
+                  ? Colors.white
+                  : const Color(0xffFDB731),
+            ),
+            Icon(
+              Icons.shopping_bag,
+              size: 30,
+              color: controller.currentBottom.value == 2
+                  ? Colors.white
+                  : const Color(0xffFDB731),
+            ),
+            Icon(
+              Icons.comment,
+              size: 30,
+              color: controller.currentBottom.value == 3
+                  ? Colors.white
+                  : const Color(0xffFDB731),
+            ),
+            Icon(
+              Icons.person,
+              size: 30,
+              color: controller.currentBottom.value == 4
+                  ? Colors.white
+                  : const Color(0xffFDB731),
+            ),
+          ],
+        );
+      }),
+
+      body: Obx(() {
+        return widgetMenuPages.elementAt(controller.currentBottom.value);
+      }),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  final controller = Get.put(HomeController(), permanent: true);
+
+  HomeWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+// *: DATA
+    final List<String> imgList = [
+      'assets/S3.png',
+      'assets/PORSOED.png',
+      'assets/nyala_desa.png',
+      'assets/FOSA.png',
+    ];
     List<Widget> imageSliders = imgList
         .map(
           (item) => ClipRRect(
@@ -33,126 +114,111 @@ class HomeView extends GetView<HomeController> {
           ),
         )
         .toList();
-    return Scaffold(
-// NOTE: BOTTOM NAVIGATION BAR
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.amber,
-        height: 50,
-        items: const [
-          Icon(Icons.home, size: 30),
-          Icon(Icons.newspaper, size: 30),
-          Icon(Icons.shopping_bag, size: 30),
-          Icon(Icons.comment, size: 30),
-          Icon(Icons.person, size: 30),
-        ],
-      ),
-
-      body: SafeArea(
-        bottom: false,
-        child: Column(
-          children: [
-            // NOTE: WELCOME USER AND NOTIFICATION ICON
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 24,
-                right: 24,
-                top: 26,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Selamat datang,\nNama User!',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                  Badge(
-                    alignment: Alignment.topRight,
-                    shape: BadgeShape.circle,
-                    badgeContent: const Text(
-                      '200',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    badgeColor: const Color(0xffF8A435),
-                    child: Image.asset(
-                      'assets/Icon-notifikasi.png',
-                      width: 30,
-                      height: 30,
-                    ),
-                  )
-                ],
-              ),
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        children: [
+          // NOTE: WELCOME USER AND NOTIFICATION ICON
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 24,
+              right: 24,
+              top: 26,
             ),
-            // NOTE: END WELCOME USER AND NOTIFICATION ICON ==============
-
-// NOTE: CAROUSEL SLIDER
-
-            Obx(() {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: CarouselSlider(
-                      carouselController: controller.carouselController,
-                      items: imageSliders,
-                      options: CarouselOptions(
-                        initialPage: controller.current.value,
-                        autoPlay: true,
-                        enlargeCenterPage: true,
-                        aspectRatio: 2.0,
-                        onPageChanged: controller.onPageChange,
-                      ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Selamat datang,\nNama User!',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Badge(
+                  alignment: Alignment.topRight,
+                  shape: BadgeShape.circle,
+                  badgeContent: const Text(
+                    '200',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(
-                    height: 4,
+                  badgeColor: const Color(0xffF8A435),
+                  child: Image.asset(
+                    'assets/Icon-notifikasi.png',
+                    width: 30,
+                    height: 30,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: imgList
-                        .asMap()
-                        .entries
-                        .map(
-                          (e) => GestureDetector(
-                            onTap: () {
-                              controller.carouselController
-                                  .animateToPage(e.key);
-                            },
-                            child: Container(
-                              width: 12,
-                              height: 12,
-                              margin: const EdgeInsets.symmetric(
-                                vertical: 8,
-                                horizontal: 4,
-                              ),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: (Theme.of(context).brightness ==
-                                            Brightness.dark
-                                        ? Colors.white
-                                        : Colors.black)
-                                    .withOpacity(
-                                  controller.current.value == e.key ? 0.9 : 0.4,
-                                ),
+                )
+              ],
+            ),
+          ),
+          // NOTE: END WELCOME USER AND NOTIFICATION ICON ==============
+
+          // NOTE: CAROUSEL SLIDER
+          Obx(() {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 24),
+                  child: CarouselSlider(
+                    carouselController: controller.carouselController,
+                    items: imageSliders,
+                    options: CarouselOptions(
+                      initialPage: controller.currentSlider.value,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      aspectRatio: 2.0,
+                      onPageChanged: controller.onPageChangeSlider,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: imgList
+                      .asMap()
+                      .entries
+                      .map(
+                        (e) => GestureDetector(
+                          onTap: () {
+                            controller.carouselController.animateToPage(e.key);
+                          },
+                          child: Container(
+                            width: 12,
+                            height: 12,
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 8,
+                              horizontal: 4,
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: (Theme.of(context).brightness ==
+                                          Brightness.dark
+                                      ? Colors.white
+                                      : Colors.black)
+                                  .withOpacity(
+                                controller.currentSlider.value == e.key
+                                    ? 0.9
+                                    : 0.4,
                               ),
                             ),
                           ),
-                        )
-                        .toList(),
-                  )
-                ],
-              );
-            }),
-            // NOTE: END CAROUSEL SLIDER =========
+                        ),
+                      )
+                      .toList(),
+                )
+              ],
+            );
+          }),
+          // NOTE: END CAROUSEL SLIDER =========
 
-            // NOTE : MAIN CONTENT
-          ],
-        ),
+          // NOTE : MAIN CONTENT
+        ],
       ),
     );
   }
