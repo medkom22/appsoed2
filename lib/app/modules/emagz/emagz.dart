459c96d06a62b8772983275030ed81b22a6c9d9b
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2022-07-02 18:06:00 +0700

    menambahkan asset thumbnail emagz

--- lib/app/modules/emagz/emagz.dart
+++ lib/app/modules/emagz/emagz.dart
@@ -1,45 +1,73 @@
-import 'dart:html';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
-import 'package:getwidget/components/accordion/gf_accordion.dart';
 import 'package:flutter/material.dart';
 
-void main() { runApp(MyApp()); }  
-  
-class MyApp extends StatelessWidget{
-  final List<Container> myList = List.generate(
-    90, 
-    (index) {
-      return Container(
-        color: Color(0xFFFEDB57),
-      );
-    },
-  );
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 24,
-              ),
-              const Center(
-                child: Text(
-                  'E-Magz',
-                    style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    ),
-                ),
-              ),
-            ],
-          ),
-       ),
-      ), 
-    );      
-  }
+void main() {
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+        title: const Text('E-Magz'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 4,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          Card(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/emagz1.PNG'),
+                  fit: BoxFit.fitWidth,
+                ),
+                border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 35,))
+              ),
+            )     
+          ),
+          Card(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
+                  fit: BoxFit.fitWidth,
+                ),
+                border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 35,))
+              ),
+            ) 
+          ),
+          Card(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/emagz3.PNG'),
+                  fit: BoxFit.fitWidth,
+                ),
+                border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 35,))
+              ),
+            ) 
+          ),
+          Card(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/emagz4.PNG'),
+                  fit: BoxFit.fitWidth,
+                ),
+                border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 35,))
+              ),
+            ) 
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+}
