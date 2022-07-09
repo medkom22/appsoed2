42541e6544029ae2a94dbc13b5df35ad0e216757
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2022-07-09 09:49:34 +0700

    update emagz.dart (solved bugs)

--- lib/app/views/views/emagz.dart
+++ lib/app/views/views/emagz.dart
@@ -1,272 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(EmagzView());
 }
 
-class MyApp extends StatefulWidget {
+class EmagzView extends StatefulWidget {
+  const EmagzView({Key? key}) : super(key: key);
   @override
-  _MyAppState createState() => _MyAppState();
+  State<EmagzView> createState() => _EmagzViewState();
 }
 
-class _MyAppState extends State<MyApp>{
-  int _currentIndex=0;
+class _EmagzViewState extends State<EmagzView> {
 
   @override
-  Widget build (BuildContext context){
-    return MaterialApp (
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tampilan Awal Emagz',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top:50, bottom: 30),
-            child: Text('E Magz',
-              style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              ),
-            ),),
-            
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 400,
-              autoPlay: false,
-              onPageChanged: (index, reason) {
-                setState(() {
-                _currentIndex = index;
-                });
-              },
-            ),
-            items: [
-              Item1(),
-              Item2(),
-              Item3(),
-              Item4(),
-            ],
-
-          ),]
-          
-        )
-      )
-    );
-  }
-}
-
-class Item1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 4,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20),
-      children: [
-        GestureDetector(
-          onTap: () => 'lib\app\views\views\faq_view.dart',
-          child: Card(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage('assets/emagz1.PNG'),
-                  fit: BoxFit.fitWidth,
-                ),
-                border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-              ),
-            )    
-          ),
+        appBar: AppBar(
+          title:Text("E-Magz")
         ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
-               fit: BoxFit.fitWidth,
+      body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 24,
+                left: 16,
+                right: 16,
+                bottom: 50,
               ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz3.PNG'),
-                fit: BoxFit.fitWidth,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'E-Magz',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: 40,
+                  ),
+                ],
               ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
             ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz4.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
+            Image.asset(
+              'assets/comingsoon.png',
+              height: 300,
+              width: double.infinity - (24 * 2),
+              fit: BoxFit.contain,
             ),
-          ) 
-        ),
-      ],
-    );
-  }
-}
-
-class Item2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 4,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20),
-      children: [
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz5.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
+            const SizedBox(
+              height: 50,
             ),
-          )     
+            const Text(
+              'Laman masih dalam perbaikan. Ditunggu ya!!',
+              style: TextStyle(fontSize: 16),
+            )
+          ],
         ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
-               fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz8.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-      ],
-    );
-  }
-}
-
-class Item3 extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-        return GridView(
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 4,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20),
-      children: [
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          )     
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz10.PNG'),
-               fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz2.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-      ],
-    );
-
-  }
-}
-
-class Item4 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-        return GridView(
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 4,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20),
-      children: [
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz13.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          )     
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz14.PNG'),
-               fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-        Card(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/emagz15.PNG'),
-                fit: BoxFit.fitWidth,
-              ),
-              border: Border(bottom: BorderSide(color: Color(0xFFFEDB57), width: 25,))
-            ),
-          ) 
-        ),
-      ],
+      ),
     );
   }
 }
