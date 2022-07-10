86a78ea0ec21c8c6fa159a9bb725cdf8693eb786
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2022-07-10 21:42:09 +0700

    Deskripsi faperta done

--- lib/app/views/views/faperta.dart
+++ lib/app/views/views/faperta.dart
@@ -79,7 +79,56 @@ class FapertaView extends GetView {
                     endIndent: 30,
                   ),
                   //END OF TITLE FAKULTAS
-                   
+
+              //DESKRIPSI FAKULTAS
+              Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 26,
+                      vertical: 28,
+                    ),
+                    child: Column(
+                      children: const [
+                        Text(
+                          'Fakultas Pertanian Universitas Jenderal Soedirman (UNSOED) merupakan salah satu lembaga pendidikan tinggi negeri yang terletak di Purwokerto, Propinsi Jawa Tengah. Lembaga pendidikan tinggi di ibukota Kabupaten Banyumas merupakan fakultas tertua dan terbanyak memiliki program studi.\n',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff505050),
+                            wordSpacing: 10,
+                          ),
+                        ),
+                        Text(
+                          'Pada saat ini terdapat 4 jurusan dengan 8 program studi S1, 3 program D3, 2 program alih jenjang dan 1 program S2. Sesuai dengan SK Mendiknas No. 045/U/2002 maka Fakultas Pertanian UNSOED menerapkan kurikulum berbasis kompetensi.\n',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff505050),
+                            wordSpacing: 10,
+                          ),
+                        ),
+                        Text(
+                          'Penerapan kurikulum tersebut menuntut ketersediaan sumberdaya manusia, sarana dan prasarana dan berbagai fasilitas penunjang yang memadai, baik secara kuantitatif maupun kualitatif.\n',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff505050),
+                            wordSpacing: 10,
+                          ),
+                        ),
+                        Text(
+                          'Berdasarkan kurikulum yang berlaku, diharapkan mahasiswa dapat menyelesaikan studinya dalam waktu 4 tahun untuk program S1, 3 tahun untuk program D3, 2 tahun untuk program alih jenjang dan program S2.',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff505050),
+                            wordSpacing: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+              ),
+              //END OF DESKRIPSI FAKULTAS
+
   
                 ],
               ),
