89c14d749ed1a0ed663ae143eb92c97aaa14bb0a
Handika Rafli A <shandika231@gmail.com>
2022-09-08 10:30:56 +0700

    change to debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:change_app_package_name/change_app_package_name.dart';
 
 import 'firebase_options.dart';
 import 'my_app.dart';
